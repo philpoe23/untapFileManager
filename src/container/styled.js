8348d4711479b1bdd5599612501229e77c278a9b
tanjimhasan <tanjim.hasan.pro@gmail.com>
2020-05-16 12:07:13 +0600

    style css update

--- src/container/styled.js
+++ src/container/styled.js
@@ -2,9 +2,7 @@ import Styled from 'styled-components';
 const Main = Styled.div`
     padding: 0px 30px;
     min-height: 750px;
-<<<<<<< HEAD
     background-color: rgb(244, 245, 247);
-=======
     .ant-skeleton-paragraph{
         margin-bottom: 0;
     }
@@ -17,6 +15,5 @@ const Main = Styled.div`
         }
     }
 
->>>>>>> 6ab08ff6a918e530242782e9000d30fb5d1f9a79
 `;
 export { Main };
