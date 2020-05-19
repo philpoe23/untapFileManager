649ea9d71b739080686dbc18e6cbb6f50a8af3c0
woadudakand <woadud@aazztech.com>
2020-05-19 15:17:17 +0600

    update avater and badge

--- src/container/ui-elements/Badge.js
+++ src/container/ui-elements/Badge.js
@@ -1,13 +1,52 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 import { PageHeader } from '../../components/page-headers/page-headers';
-import { Row, Col, Badge } from 'antd';
+import { Row, Col, Badge, Switch } from 'antd';
 import { Main } from '../styled';
 import { Cards } from '../../components/cards/frame/cards-frame';
 import { BadgeWraperStyle } from './ui-elements-styled';
-import { NavLink } from 'react-router-dom';
-import { ClockCircleOutlined } from '@ant-design/icons';
+import { NavLink, Link } from 'react-router-dom';
+import { ClockCircleOutlined, NotificationOutlined, MinusOutlined, PlusOutlined } from '@ant-design/icons';
+import { BtnGroup, Button } from '../../components/buttons/buttons';
+
+const colors = [
+  'pink',
+  'red',
+  'yellow',
+  'orange',
+  'cyan',
+  'green',
+  'blue',
+  'purple',
+  'geekblue',
+  'magenta',
+  'volcano',
+  'gold',
+  'lime',
+];
 
 const Badges = () => {
+  const [state, setState] = useState({
+    count: 5,
+    show: true,
+  });
+
+  const increase = () => {
+    const count = state.count + 1;
+    setState({ ...state, count });
+  };
+
+  const decline = () => {
+    let count = state.count - 1;
+    if (count < 0) {
+      count = 0;
+    }
+    setState({ ...state, count });
+  };
+
+  const onChange = show => {
+    setState({ ...state, show });
+  };
+
   return (
     <Fragment>
       <PageHeader ghost title="Badge" />
@@ -61,6 +100,78 @@ const Badges = () => {
                 <Badge count={1000} overflowCount={999}>
                   <NavLink to="#" className="head-example" />
                 </Badge>
+              </BadgeWraperStyle>
+            </Cards>
+          </Col>
+
+          <Col md={12}>
+            <Cards title="Red badge">
+              <BadgeWraperStyle>
+                <Badge dot>
+                  <NotificationOutlined />
+                </Badge>
+                <Badge count={0} dot>
+                  <NotificationOutlined />
+                </Badge>
+                <Badge dot>
+                  <Link to="#">Link something</Link>
+                </Badge>
+              </BadgeWraperStyle>
+            </Cards>
+          </Col>
+
+          <Col md={12}>
+            <Cards title="Clickable">
+              <BadgeWraperStyle>
+                <Link to="#">
+                  <Badge count={5}>
+                    <span className="head-example" />
+                  </Badge>
+                </Link>
+              </BadgeWraperStyle>
+            </Cards>
+          </Col>
+
+          <Col md={12}>
+            <Cards title="Dynamic">
+              <BadgeWraperStyle>
+                <div>
+                  <Badge count={state.count}>
+                    <Link to="#" className="head-example" />
+                  </Badge>
+                  <BtnGroup>
+                    <Button onClick={decline}>
+                      <MinusOutlined />
+                    </Button>
+                    <Button onClick={increase}>
+                      <PlusOutlined />
+                    </Button>
+                  </BtnGroup>
+                </div>
+                <div style={{ marginTop: 10 }}>
+                  <Badge dot={state.show}>
+                    <Link to="#" className="head-example" />
+                  </Badge>
+                  <Switch onChange={onChange} checked={state.show} />
+                </div>
+              </BadgeWraperStyle>
+            </Cards>
+          </Col>
+          <Col md={12}>
+            <Cards title="Status">
+              <BadgeWraperStyle>
+                <div>
+                  {colors.map(color => (
+                    <Badge key={color} color={color} />
+                  ))}
+                </div>
+                <div>
+                  {colors.map(color => (
+                    <div key={color}>
+                      <Badge color={color} text={color} />
+                    </div>
+                  ))}
+                </div>
               </BadgeWraperStyle>
             </Cards>
           </Col>
