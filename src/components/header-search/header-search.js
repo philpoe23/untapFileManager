1172f248b2b0fe397d84babb58ff00442fd08f92
woadudakand <woadud@aazztech.com>
2020-06-16 08:46:46 +0600

    header search controll in mobile screen

--- src/components/header-search/header-search.js
+++ src/components/header-search/header-search.js
@@ -1,18 +1,44 @@
-import React from 'react';
+import React, { Fragment, useLayoutEffect, useState } from 'react';
 import { Icon, Input, Row, Col } from 'antd';
 import { Div } from './header-search-style';
-import { NavLink } from 'react-router-dom';
+import { NavLink, Link } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { headerSearchAction } from '../../redux/headerSearch/actionCreator';
 import PropTypes from 'prop-types';
 import { Popover } from '../popup/popup';
+import { Button } from '../buttons/buttons';
+import FeatherIcon from 'feather-icons-react';
 
 const HeaderSearch = props => {
   const dispatch = useDispatch();
   const searchData = useSelector(state => state.headerSearchData);
 
+  const [state, setState] = useState({
+    size: 0,
+    hide: false,
+  });
+
+  const { size, hide } = state;
+
+  useLayoutEffect(() => {
+    function updateSize() {
+      setState({ hide: window.innerWidth <= 600 ? true : false, size: window.innerWidth });
+    }
+    window.addEventListener('resize', updateSize);
+    updateSize();
+    return () => window.removeEventListener('resize', updateSize);
+  }, []);
+
   const search = e => {
     dispatch(headerSearchAction(e.target.value));
+  };
+
+  const handleHide = e => {
+    e.preventDefault();
+    setState({
+      ...state,
+      hide: hide ? false : true,
+    });
   };
 
   const content = (
@@ -34,18 +60,27 @@ const HeaderSearch = props => {
   );
 
   return (
-    <Div className="certain-category-search-wrapper" style={{ width: '100%' }}>
-      <Row>
-        <Col xs={2} className="text-right">
-          <Icon type="search" className="certain-category-icon" />
-        </Col>
-        <Col xs={22}>
-          <Popover placement="bottomLeft" content={content} title="Search List" trigger="focus">
-            <Input placeholder="Search..." onInput={search} />
-          </Popover>
-        </Col>
-      </Row>
-    </Div>
+    <Fragment>
+      {!hide && (
+        <Div className="certain-category-search-wrapper" style={{ width: '100%' }}>
+          <Row>
+            <Col xs={2} className="text-right">
+              <Icon type="search" className="certain-category-icon" />
+            </Col>
+            <Col xs={22}>
+              <Popover placement="bottomLeft" content={content} title="Search List" trigger="focus">
+                <Input placeholder="Search..." onInput={search} />
+              </Popover>
+            </Col>
+          </Row>
+        </Div>
+      )}
+      {size <= 600 && (
+        <Link onClick={handleHide} to="#">
+          <FeatherIcon icon="search" />
+        </Link>
+      )}
+    </Fragment>
   );
 };
 
