c64038fb079f9b43d08bde55691c613230eee2b5
woadudakand <alwaysitlab@gmail.com>
2020-09-13 10:52:59 +0600

    update vector map

--- src/container/maps/vector.js
+++ src/container/maps/vector.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import { Row, Col } from 'antd';
-import { VectorMap } from '@south-paw/react-vector-maps';
+import { VectorMap } from 'react-jvectormap';
 import { PageHeader } from '../../components/page-headers/page-headers';
 import { Cards } from '../../components/cards/frame/cards-frame';
 import { Main } from '../styled';
-import worldLowRes from '../../demoData/vector.json';
-import config from '../../config/config';
+// import config from '../../config/config';
 
-const { theme } = config;
+// const { theme } = config;
 
 const VectorMaps = () => {
   return (
@@ -18,7 +17,34 @@ const VectorMaps = () => {
           <Col xs={24}>
             <div className="vertor-map">
               <Cards title="World Map" size="large">
-                <VectorMap {...worldLowRes} fill={theme['primary-color']} stroke="white" />
+                <div style={{ width: 400, height: 450 }}>
+                  <VectorMap
+                    map="world_mill"
+                    backgroundColor="transparent"
+                    regionStyle={{
+                      initial: {
+                        fill: 'red',
+                        'fill-opacity': 1,
+                        stroke: 'none',
+                        'stroke-width': 0,
+                        'stroke-opacity': 1,
+                      },
+                      hover: {
+                        'fill-opacity': 0.8,
+                        cursor: 'pointer',
+                      },
+                      selected: {
+                        fill: 'yellow',
+                      },
+                      selectedHover: {},
+                    }}
+                    containerStyle={{
+                      width: '100%',
+                      height: '100%',
+                    }}
+                    containerClassName="map"
+                  />
+                </div>
               </Cards>
             </div>
           </Col>
