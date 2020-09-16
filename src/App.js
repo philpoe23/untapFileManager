275bd818080a10c27df2afe73e865dae8a7451f5
woadudakand <alwaysitlab@gmail.com>
2020-09-16 14:19:09 +0600

    working with rtl

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { hot } from 'react-hot-loader/root';
-import { Provider } from 'react-redux';
+import { Provider, useSelector } from 'react-redux';
 import { ThemeProvider } from 'styled-components';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import store from './redux/store';
@@ -10,19 +10,32 @@ import Auth from './routes/auth';
 import './static/css/style.css';
 import config from './config/config';
 import ProtectedRoute from './components/utilities/protectedRoute';
+import rtl from "styled-components-rtl";
 
 const { theme } = config;
 
+const ProviderConfig = () => {
+  const { rtl } = useSelector(state => {
+    return {
+      rtl: state.ChangeLayoutMode.rtlData,
+    };
+  });
+
+  return (
+    <ThemeProvider theme={{ ...theme, rtl }}>
+      <Router basename={process.env.PUBLIC_URL}>
+        <Route exact path="/" component={Auth} />
+        <ProtectedRoute path="/admin" component={Admin} />
+      </Router>
+    </ThemeProvider>
+  );
+};
+
 function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <Provider store={store}>
-        <Router basename={process.env.PUBLIC_URL}>
-          <Route exact path="/" component={Auth} />
-          <ProtectedRoute path="/admin" component={Admin} />
-        </Router>
-      </Provider>
-    </ThemeProvider>
+    <Provider store={store}>
+      <ProviderConfig />
+    </Provider>
   );
 }
 
