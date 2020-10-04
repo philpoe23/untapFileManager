d274b3d0d7c64001b748ca5718c46f13273d1d95
woadudakand <alwaysitlab@gmail.com>
2020-10-04 16:07:20 +0600

    update login page logo

--- src/container/profile/authentication/Index.js
+++ src/container/profile/authentication/Index.js
@@ -13,7 +13,7 @@ const AuthLayout = WraperContent => {
               <img src={require('../../../static/img/auth/topShape.png')} alt="" className="topShape" />
               <img src={require('../../../static/img/auth/bottomShape.png')} alt="" className="bottomShape" />
               <Content>
-                <img src={require('../../../static/img/logo.png')} alt="" />
+                <img style={{ width: '150px' }} src={require('../../../static/img/Logo_Dark.svg')} alt="" />
                 <br />
                 <br />
                 <Heading as="h1">
