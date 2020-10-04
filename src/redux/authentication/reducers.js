e98d8fcd99353cb347ca2b72925a37a3f37255c4
woadudakand <alwaysitlab@gmail.com>
2020-10-04 10:19:18 +0600

    Cookies update

--- src/redux/authentication/reducers.js
+++ src/redux/authentication/reducers.js
@@ -1,9 +1,10 @@
+import Cookies from 'js-cookie';
 import actions from './actions';
 
 const { LOGIN_BEGIN, LOGIN_SUCCESS, LOGIN_ERR, LOGOUT_BEGIN, LOGOUT_SUCCESS, LOGOUT_ERR } = actions;
 
 const initState = {
-  login: window.localStorage.getItem('logedIn'),
+  login: Cookies.get('logedIn'),
   loading: false,
   error: null,
 };
